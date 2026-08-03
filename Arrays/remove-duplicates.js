188c96127748ccdc375e960fe8c3f5c4f636111e
ajeyajaz <ajeyajaz@gmail.com>
2026-08-03 12:04:09 +0530

    Remove Duplicates from Sorted Array

---
+++ Arrays/remove-duplicates.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+//  Time Complexity O(N)
+// Space Complexity O(1)
+
+var removeDuplicates = function(nums) {
+
+    let k = 0;
+    for(let i = 0; i < nums.length; i++){
+
+        if(nums[k] !== nums[i]){
+            nums[++k] = nums[i];
+        }
+
+        
+    }
+    return k+1;
+};
